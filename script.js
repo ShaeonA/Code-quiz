7e4f019d60520ace3b8879b4de05a8ce4b6f254f
ShaeonA <Shaeonada@gmail.com>
2021-05-27 03:30:21 -0400

    set up function for question choices and added timer in html

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var infoBox = document.querySelector(".info-box")
 var continuebtn = document.querySelector(".restart")
 var quizque = document.querySelector(".title")
 var quizans = document.querySelector(".choices")
+var counterque = 0 
+var start = 15
+
+
 console.log(infoBox)
 var questionsArr = [
     { questionTitle : "is the sky blue?",
@@ -13,13 +17,23 @@ var questionsArr = [
 ]
 
 function continueOn(){
+    
     infoBox.style.display = "none";
-    quizque.textContent = questionsArr[0].questionTitle
-    quizque.textContent = questionsArr[0].questionChoices
-  
-
+    quizque.textContent = questionsArr[0].questionTitle;
+    quizans.textContent = questionsArr[0].questionChoices;
+    // check if there are more questions or if this is the last questiion. 
+    // if not at the end do the DOM display to ask the question . 
+    for (var end = 0; end < questionsArr[counterque].questionChoices.length; end++);
+   //if the question timer is greater than zero, add to the variable end. 
 
 }
+
+// create a function to attach to choices.
+function qchoices () {
+    
+}
+// Inside function acdess current question, figure out what choice was made or if it is right or wrong. 
+
 
 startbtn.addEventListener("click", function(){
     // alert("I have been clicked");
