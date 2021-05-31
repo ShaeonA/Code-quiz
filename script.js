eaea49634de7872123ee83416620ae5f3d047566
ShaeonA <Shaeonada@gmail.com>
2021-05-31 17:08:05 -0400

    set up structure for checkAnswers func

--- script.js
+++ script.js
@@ -3,43 +3,52 @@ var infoBox = document.querySelector(".info-box")
 var continuebtn = document.querySelector(".restart")
 var quizque = document.querySelector(".title")
 var quizans = document.querySelector(".choices")
-var counterque = 0 
+var counterque = 0
 var start = 15
 
 
-console.log(infoBox)
+
+//console.log(infoBox)
 var questionsArr = [
-    { questionTitle : "is the sky blue?",
-      questionChoices : ["yes","no"],
-      answers : ["yes"]
+    {
+        questionTitle: "is the sky blue?",
+        questionChoices: [
+            'A : Yes',
+            'B : No '],
+        answers: ["yes"]
     },
-  
+
 ]
 
-function continueOn(){
+
+function checkAnswers() {
     
+}
+
+function continueOn() {
+
     infoBox.style.display = "none";
     quizque.textContent = questionsArr[0].questionTitle;
     quizans.textContent = questionsArr[0].questionChoices;
     // check if there are more questions or if this is the last questiion. 
     // if not at the end do the DOM display to ask the question . 
     for (var end = 0; end < questionsArr[counterque].questionChoices.length; end++);
-   //if the question timer is greater than zero, add to the variable end. 
+    //if the question timer is greater than zero, add to the variable end. 
 
 }
 
 // create a function to attach to choices.
-function qchoices () {
+function qchoices() {
     var curquest = document.querySelector(".choices")
 }
 // Inside function access current question, figure out what choice was made or if it is right or wrong. 
 
 
-startbtn.addEventListener("click", function(){
+startbtn.addEventListener("click", function () {
     // alert("I have been clicked");
     startbtn.style.display = "none";
     infoBox.style.display = "block";
-    
+
 
 })
 continuebtn.addEventListener("click", continueOn)
